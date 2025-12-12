5b0f1adbf3f82760a881dfad2574fa7d51d5b422
tanakadays <165740240+tanakadays@users.noreply.github.com>
2025-12-12 11:29:38 +0900

    Estimate pi via the volume of a sphere

--- pi.R
+++ pi.R
@@ -1,7 +1,15 @@
-# For fun only.
+# For fun only. 
 # See: https://academo.org/demos/estimating-pi-monte-carlo/
 
 Nreps <- 1000000
 x     <- runif(Nreps, -1, 1)
 y     <- runif(Nreps, -1, 1)
 mean(x^2 + y^2 < 1) * 4
+
+# We can also try using the formula of the volume of a sphere.
+# For radius 1, the volume is (4/3)*pi.
+# So the following should also work to approx. pi:
+x     <- runif(Nreps, -1, 1)
+y     <- runif(Nreps, -1, 1)
+z     <- runif(Nreps, -1, 1)
+mean(x^2 + y^2 + z^2 < 1) * 6
