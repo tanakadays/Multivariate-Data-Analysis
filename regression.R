4f94cbd60829e6073251ff9c0c48e52c91492e43
tanakadays <165740240+tanakadays@users.noreply.github.com>
2025-12-12 10:19:06 +0900

    Trying out Bayesian regression

--- regression.R
+++ regression.R
@@ -8,3 +8,9 @@ summary(reg.output)
 
 # Plot y against x:
 plot(mydata$x, mydata$y)
+
+# Humm, let's try to use Bayesian regression, not sure....
+library(rstanarm) # allows fitting Bayesian models, the easy way
+bayesreg.output <- stan_glm(y ~ x, family = gaussian(), mydata)
+summary(bayesreg.output)
+plot(bayesreg.output)
